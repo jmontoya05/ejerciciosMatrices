5aae2d905f7f34b92095f8421ee712ecf4d5aee8
jmontoya05 <santiagomontoya997@gmail.com>
2023-08-17 10:51:08 -0500

    se finaliza el ejercicio # 6

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -62,6 +61,56 @@ public class Main {
         llenarMatrizTemperatura(matrizTemp, filas, columnas);
         calcularTemperaturas(matrizTemp, filas, columnas);
 
+        //Ejercicio # 6
+        System.out.println("Ejercicio # 6 (Ventas fábrica)");
+        int opcionUsuario;
+        columnas = 5;
+        filas = 12;
+        matriz = new int[filas][columnas];
+        boolean seguir = true;
+
+        llenarMatrizVentas(matriz, filas, columnas);
+
+        while (seguir){
+
+            System.out.println("Selecciones la opción que desea visulizar\n");
+            System.out.println("1 para saber las ventas mensuales de la fábrica");
+            System.out.println("2 para ver el monto anual");
+            System.out.println("3 para obtener el monto del departamento que tuvo la mayor venta en el mes de julio");
+            System.out.println("4 para obtener la mayor venta de un departamento");
+            System.out.println("0 para salir");
+            opcionUsuario = sc.nextInt();
+
+            switch (opcionUsuario){
+                case 0:
+                    seguir = false;
+                    break;
+                case 1:
+                    ventasMensuales(matriz, filas, columnas);
+                    break;
+                case 2:
+                    ventasAnuales(matriz, filas, columnas);
+                    break;
+                case 3:
+                    mayorVentasJulio(matriz, columnas);
+                    break;
+                case 4:
+
+                    System.out.println("Ingrese un número de departamento para validar su mayor venta (Entre 1 y 5)");
+                    int departamento = sc.nextInt();
+
+                    if (departamento >= 1 && departamento <= 5){
+                        mayorVentaDepartamento(matriz,  filas, departamento);
+                    } else {
+                        System.out.println("Ingresó un departamento incorrecto\n");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Ingresó una opción incorrecta, intentelo nuevamente\n");
+
+            }
+        }
     }
 
     //Metodo para llenar la matriz con números aleatorios entre 1 y 9
@@ -144,9 +193,9 @@ public class Main {
 
                 if (matrizTemp[i][j] - (int) matrizTemp[i][j] == 0) {
                     if (matrizTemp[i][j] < 10) {
-                        System.out.print("| " + (int)matrizTemp[i][j] + "    |");
+                        System.out.print("| " + (int) matrizTemp[i][j] + "    |");
                     } else {
-                        System.out.print("| " + (int)matrizTemp[i][j] + "   |");
+                        System.out.print("| " + (int) matrizTemp[i][j] + "   |");
                     }
                 } else {
                     if (matrizTemp[i][j] < 10) {
@@ -212,12 +261,12 @@ public class Main {
                 promedioTemp = sumaTemp / columnas;
             }
 
-            System.out.println("La temperatura mayor de la semana " + (i+1) + " fue de " + tempMayor + " registrada el día " + valida_dia(diaMayor));
-            System.out.println("La temperatura menor de la semana " + (i+1) + " fue de " + tempMenor + " registrada el día " + valida_dia(diaMenor));
-            System.out.println("El promedio de temperatura de la semana " + (i+1) + " fue de " + promedioTemp);
+            System.out.println("La temperatura mayor de la semana " + (i + 1) + " fue de " + tempMayor + " registrada el día " + valida_dia(diaMayor));
+            System.out.println("La temperatura menor de la semana " + (i + 1) + " fue de " + tempMenor + " registrada el día " + valida_dia(diaMenor));
+            System.out.println("El promedio de temperatura de la semana " + (i + 1) + " fue de " + promedioTemp);
             System.out.println("------");
         }
-        System.out.println("La temperatura más alta del mes fue de " + tempMayorMes + "  registrada el día " + valida_dia(diaMayorMes) + " de la semana " + (semanaMayorMes + 1) );
+        System.out.println("La temperatura más alta del mes fue de " + tempMayorMes + "  registrada el día " + valida_dia(diaMayorMes) + " de la semana " + (semanaMayorMes + 1));
     }
 
     //Metodo para devolver el día de la semana de acuerdo al número
@@ -239,6 +288,62 @@ public class Main {
             default:
                 return "Domingo";
         }
+    }
+
+    private static void llenarMatrizVentas(int[][] matriz, int filas, int columnas) {
+        System.out.println("Mes/Depto     1     2     3     4     5");
+        for (int i = 0; i < filas; i++) {
+            System.out.print((i + 1) + "  |          ");
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j] = (int) Math.floor(Math.random() * 900 + 100);
+                System.out.print(matriz[i][j] + "   ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void ventasMensuales(int[][] matriz, int filas, int columnas) {
+        int ventasMes;
+        for (int i = 0; i < filas; i++) {
+            ventasMes = 0;
+            for (int j = 0; j < columnas; j++) {
+                ventasMes += matriz[i][j];
+            }
+            System.out.println("Las ventas del mes " + (i + 1) + " fueron de " + ventasMes);
+        }
+    }
+
+    public static void ventasAnuales(int[][] matriz, int filas, int columnas) {
+        int ventasAnio = 0;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                ventasAnio += matriz[i][j];
+            }
+        }
+        System.out.println("El monto total de las ventas anuales fue de " + ventasAnio);
+    }
+
+    public static void mayorVentasJulio(int[][] matriz, int columnas) {
+        int departamentoVentaMayor = 0, ventaMayor, mes = 6;
+        ventaMayor = matriz[mes][departamentoVentaMayor];
+        for (int i = 1; i < columnas; i++) {
+            if (matriz[mes][i] > matriz[mes][departamentoVentaMayor]){
+                departamentoVentaMayor = i;
+                ventaMayor = matriz[mes][i];
+            }
+        }
+        System.out.println("El departamento con mayor ventas en julio es el número " + (departamentoVentaMayor+1) + " con una venta de " + ventaMayor);
+    }
+
+    public static void mayorVentaDepartamento(int[][] matriz, int filas, int departamento) {
+        int ventaMayor = 0, mesMayor = 0;
+        for (int i = 0; i < filas; i++) {
+            if (matriz[i][departamento - 1] > ventaMayor){
+                ventaMayor = matriz[i][departamento - 1];
+                mesMayor = i;
+            }
+        }
+        System.out.println("La venta mayor del departamento número " + departamento + " fue de " + ventaMayor + " en el mes " + (mesMayor+1));
     }
 
 }
